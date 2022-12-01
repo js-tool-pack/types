bdfce18db1aff8294dfbcca24d72d5cc2983ae03
dyh_a <xinzon32177@163.com>
2022-12-01 23:45:46 +0800

    docs: 文档按文件类型分类成功

---
+++ src/index.typedoc.ts
@@ -0,0 +1,20 @@
+/**
+ * 元组相关类型工具
+ */
+export * as Tuple from './tuple';
+/**
+ * 字符串相关类型工具
+ */
+export * as String from './string';
+/**
+ * 对象相关类型工具
+ */
+export * as Object from './object';
+/**
+ * dom相关类型工具
+ */
+export * as Dom from './dom';
+/**
+ * 对象路径类型工具
+ */
+export * as ObjectPath from './object-path';

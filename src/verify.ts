b44d97c00f8bced2928b234431b1b5518dd5a798
dyh_a <xinzon32177@163.com>
2023-04-01 15:55:50 +0800

    feat(verify): `IsAny`

--- src/verify.ts
+++ src/verify.ts
@@ -1,3 +1,8 @@
+import type { AntiBool } from './utils';
+
+type IsUnknownExtend<T, B extends boolean> = unknown extends T ? B : false;
+type IsUnknownLike<T> = [keyof T] extends [never] ? (never extends T ? true : false) : false;
+
 /**
  * 判断一个类型是否是unknown
  *
@@ -8,10 +13,19 @@
  * type t4 = IsUnknown<boolean>; // false
  * type t5 = IsUnknown<''>; // false
  */
-export type IsUnknown<T> = unknown extends T
-  ? [keyof T] extends [never]
-    ? never extends T
-      ? true
-      : false
-    : false
-  : false;
+export type IsUnknown<T> = IsUnknownExtend<T, IsUnknownLike<T>>;
+
+/**
+ * 判断一个类型是否是any
+ *
+ * @example
+ * type T = IsAny<any>; // true
+ * type T2 = IsAny<number>; // false
+ * type t3 = IsAny<{}>; // false
+ * type t4 = IsAny<never>; // false
+ * type t5 = IsAny<undefined>; // false
+ * type t6 = IsAny<null>; // false
+ * type t7 = IsAny<unknown>; // false
+ *
+ */
+export type IsAny<T> = IsUnknownExtend<T, AntiBool<IsUnknownLike<T>>>;
